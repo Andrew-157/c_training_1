357ab679164ac23b3377ece1abc30d7c9e33e982
Andrew Subotin <subotinandrey5@gmail.com>
2024-04-08 21:41:42 +0200

    Minor modifications

--- main.c
+++ main.c
@@ -2,7 +2,13 @@
 
 int main()
 {
-    printf("Hello World");
+    int myFavoriteNumber = 0;
+
+    printf("Please, enter your favorite number: ");
+
+    scanf("%d", &myFavoriteNumber);
+
+    printf("Your favorite number is: %d\n", myFavoriteNumber);
 
     return 0;
 }
