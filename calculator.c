d32839aa6963a085aff6c513218ee9af23494728
Andrew Subotin <subotinandrey5@gmail.com>
2024-07-02 20:42:11 +0200

    Wrote primitive calculator

---
+++ calculator.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+char OPERATORS[] = {'+', '-', '*', '/'};
+
+bool is_valid_operator(char operator)
+{
+    int length = sizeof(OPERATORS) / sizeof(OPERATORS[0]);
+    for (int i = 0; i < length; i++)
+    {
+        if (operator== OPERATORS[i])
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+int main()
+{
+
+    int operand1, operand2;
+    char operator;
+
+    while (true)
+    {
+
+        printf("Enter first operand: ");
+        scanf(" %d", &operand1);
+
+        printf("Enter Operator: ");
+        scanf(" %c", &operator);
+        while (!(is_valid_operator(operator)))
+        {
+            printf("ERROR. Not valid operator. Valid choices are: {\"+\", \"-\", \"*\", \"/\"}}\n");
+            printf("Enter operator again: ");
+            scanf(" %c", &operator);
+        }
+
+        printf("Enter second operand: ");
+        scanf(" %d", &operand2);
+
+        int result;
+        if (operator== '+')
+        {
+            result = operand1 + operand2;
+        }
+        else if (operator== '-')
+        {
+            result = operand1 - operand2;
+        }
+        else if (operator== '*')
+        {
+            result = operand1 * operand2;
+        }
+        else
+        {
+            result = operand1 / operand2;
+        }
+
+        printf("The result of %d %c %d is %d\n", operand1, operator, operand2, result);
+    }
+
+    return 0;
+}
--- test.c
+++ test.c
@@ -1,56 +1,17 @@
 #include <stdio.h>
-#include <stdbool.h>
 
-// utility function to check if the integer is positive
-bool is_positive(int number)
-{
-    if (number >= 0)
-    {
-        return true;
-    }
-    return false;
-}
-
-// utility function to check if the integer is even
-bool is_even(int number)
-{
-    if ((number & 1) > 0)
-    {
-        return false;
-    }
-    return true;
-}
-
+// driver code
 int main()
 {
-    int x = 8;
+    char a[50];
+    printf("Enter a word: ");
+    scanf("%s", &a);
+    printf("Entered word: %s\n", a);
 
-    char success_message[] = "At least one condition is true\n";
-    char failure_message[] = "Conditions are not satisfied\n";
-
-    // The first condition is evaluated and found to be true,
-    // so the second condition is not evaluated
-    if (is_positive(x) || is_even(x))
-    {
-        printf("%s", success_message);
-    }
-    else
-    {
-        printf("%s", failure_message);
-    }
-
-    signed int y = -5;
-
-    // The first condition is evaluated and found to be false,
-    // so the second condition is evaluated
-    if (is_positive(y) || is_even(y))
-    {
-        printf("%s", success_message);
-    }
-    else
-    {
-        printf("%s", failure_message);
-    }
+    char b[50];
+    printf("Enter a sentence: ");
+    scanf(" %[^\n]s", b);
+    printf("Entered sentence: %s", b);
 
     return 0;
 }
